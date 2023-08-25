3b276786585085103221421c9fb333418703bb34
Тимофеев Владимир <mr.vufik@gmail.com>
2023-08-25 23:00:26 +0300

    Дописан метод, для нахождения количества чисел, являющимися квадратами целых чисел :)

--- myPr/PositiveEvenFinder/src/PositiveEvenFinder.java
+++ myPr/PositiveEvenFinder/src/PositiveEvenFinder.java
@@ -5,6 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PositiveEvenFinder {
+    public static int countPerfectSquares(List<Integer> numbers) {
+        int count = 0;
+        for (int number : numbers) {
+            if (number > 0) {
+                double sqrt = Math.sqrt(number);
+                if (sqrt == (int) sqrt) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         File file = new File("src/resources/numbers.txt");
         List<Integer> positiveEvenNumbers = new ArrayList<>();
@@ -19,7 +32,7 @@ public class PositiveEvenFinder {
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден: " + e.getMessage());
         }
-
+        System.out.println("Количество чисел, являющимися квадратами целых чисел: " + countPerfectSquares(positiveEvenNumbers));
         System.out.println("Положительные и чётные числа из файла:");
         for (int number : positiveEvenNumbers) {
             System.out.println(number);
